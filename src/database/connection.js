a26fc3b6c4b891a0180905f1a085a7ff6ecad19e
Surya K S <svprojectsworks@gmail.com>
2026-06-12 22:03:41 +0530

    feat: implement dropStaleIndexes function to clean up outdated MongoDB indexes

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,10 +2,25 @@ const mongoose = require("mongoose");
 const env = require("../config/env");
 const logger = require("../config/logger");
 
+const dropStaleIndexes = async () => {
+  try {
+    const rolesCollection = mongoose.connection.collection("roles");
+    const indexes = await rolesCollection.indexes();
+    const stale = indexes.find((idx) => idx.name === "restaurantId_1_name_1");
+    if (stale) {
+      await rolesCollection.dropIndex("restaurantId_1_name_1");
+      logger.info("Dropped stale index: roles.restaurantId_1_name_1");
+    }
+  } catch (err) {
+    logger.warn(`Index cleanup skipped: ${err.message}`);
+  }
+};
+
 const connectDatabase = async () => {
   mongoose.set("strictQuery", true);
   await mongoose.connect(env.mongoUri);
   logger.info("MongoDB connected");
+  await dropStaleIndexes();
 };
 
 module.exports = connectDatabase;
