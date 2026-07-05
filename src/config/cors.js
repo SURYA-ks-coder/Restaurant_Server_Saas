a6ab7a1bd732ff93bfabaaabea249a8f1a21435b
Surya K S <svprojectsworks@gmail.com>
2026-07-05 21:08:07 +0530

    feat: add additional allowed origin for CORS configuration

--- src/config/cors.js
+++ src/config/cors.js
@@ -7,6 +7,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://restaurant-ui-saas-ruddy.vercel.app/",
+  "https://my-app-livid-ten-32.vercel.app/",
 ]
   .filter(Boolean)
   .map(normalize);
